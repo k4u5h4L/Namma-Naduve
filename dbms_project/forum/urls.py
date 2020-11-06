8cb9e8d09a39c1601088bba5694f44cebd44ea56
k4u5h4L <kaushal.v.bhat@gmail.com>
2020-11-06 20:29:44 +0530

    add index views

--- dbms_project/forum/urls.py
+++ dbms_project/forum/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    path('faq/', views.faqPage, name='faqPage'),
 ]
--- dbms_project/forum/views.py
+++ dbms_project/forum/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.defaults import page_not_found 
 
 # Create your views here.
 
 def homePage(request):
     return render(request, 'forum/index.html')
+
+def faqPage(request):
+    return render(request, 'forum/faq.html')
