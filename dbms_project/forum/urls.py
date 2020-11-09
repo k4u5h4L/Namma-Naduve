aa458a9a920d651841f56c8144e103ea130638c4
k4u5h4L <kaushal.v.bhat@gmail.com>
2020-11-09 17:36:54 +0530

    add search page template

--- dbms_project/forum/urls.py
+++ dbms_project/forum/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    path('search=<str:query>/', views.searchResults, name='searchResults'),
     path('faq/', views.faqPage, name='faqPage'),
 ]
--- dbms_project/forum/views.py
+++ dbms_project/forum/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from django.views.defaults import page_not_found 
+from django.views.defaults import page_not_found
 
 # Create your views here.
+
 
 def homePage(request):
     return render(request, 'forum/index.html')
 
+
 def faqPage(request):
     return render(request, 'forum/faq.html')
 
+
+def searchResults(request, query):
+    print(query)
+    return render(request, 'forum/search.html')
+
+
 def notFoundPage(request, exception):
     return render(request, 'forum/404.html')
-
