9271ef8ddd25ab50e363f503e396d68d32f021f2
k4u5h4L <kaushal.v.bhat@gmail.com>
2020-11-09 20:31:15 +0530

    fix hyperlinks

--- dbms_project/forum/urls.py
+++ dbms_project/forum/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
-    path('search=<str:query>/', views.searchResults, name='searchResults'),
-    path('post=<int:post_id>/', views.postPage, name='postPage'),
-    path('user=<str:usr_name>/', views.profilePage, name='profilePage'),
+    path('search/<str:query>/', views.searchResults, name='searchResults'),
+    path('post/<int:post_id>/', views.postPage, name='postPage'),
+    path('user/<str:usr_name>/', views.profilePage, name='profilePage'),
     path('faq/', views.faqPage, name='faqPage'),
 ]
