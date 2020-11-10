4d4364c17ac2427692da464bbc1d54255759ddb0
k4u5h4L <kaushal.v.bhat@gmail.com>
2020-11-10 13:10:57 +0530

    Add tables to admin

--- dbms_project/forum/admin.py
+++ dbms_project/forum/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post, Reply, Tag
 
 # Register your models here.
+
+
+admin.site.register(Post)
+admin.site.register(Reply)
+admin.site.register(Tag)
