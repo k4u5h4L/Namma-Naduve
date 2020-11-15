c4e2a35602314ab6ec45367d4bede94427911599
k4u5h4L <kaushal.v.bhat@gmail.com>
2020-11-15 17:21:35 +0530

    boilerplate to create posts

--- dbms_project/forum/urls.py
+++ dbms_project/forum/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('search/', views.search_results, name='search_results'),
     path('post/<int:post_id>/', views.post_page, name='post_page'),
+    path('create/post/', views.create_post, name='create_post_page'),
     path('user/about/<str:usr_name>/',
          views.profile_about_page, name='profile_about_page'),
     path('user/<str:usr_name>/', views.profile_page, name='profile_page'),
--- dbms_project/forum/views.py
+++ dbms_project/forum/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.defaults import page_not_found
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.contrib import messages
 
 from .models import Post, Reply, Tag, CustomUser
 from .filters import PostFilter
+
+from .forms import PostForm
 
 
 # Create your views here.
@@ -11,11 +15,14 @@ from .filters import PostFilter
 @login_required
 def home_page(request):
 
+    form = PostForm()
+
     posts = Post.objects.all().order_by('-post_timestamp')
     posts_count = posts.count()
 
     context = {
         'posts_count': posts_count,
+        'form': form,
     }
 
     replies = []
@@ -44,24 +51,61 @@ def home_page(request):
 
 @login_required
 def profile_page(request, usr_name):
-    print(usr_name)
+    try:
+        user_page = CustomUser.objects.get(username=usr_name)
+    except CustomUser.DoesNotExist:
+        raise Http404("No CustomUser matches the given query.")
+
+    posts = Post.objects.filter(author=user_page)
+    # print(posts)
+
+    context = {
+        'user_page': user_page,
+        # 'user_posts': posts,
+    }
+
+    replies = []
+    context['posts'] = []
+
+    for post in posts:
+        replies.append(post.reply_set.all()[:2])
+
+    posts = [post for post in posts]
+    for i in range(len(posts)):
+        context['posts'].append(
+            {'post': posts[i], 'replies': replies[i]})
+
     # fetch user with the username variable and display that to user
-    return render(request, 'forum/profile.html')
+
+    user_posts_count = Post.objects.filter(author=request.user).count()
+    user_replies_count = Reply.objects.filter(author=request.user).count()
+
+    context['user_posts_count'] = user_posts_count
+    context['user_replies_count'] = user_replies_count
+
+    print(context)
+
+    return render(request, 'forum/profile.html', context)
 
 
 @login_required
 def profile_about_page(request, usr_name):
     print(usr_name)
     # fetch user with the username variable and display that to user
-    return render(request, 'forum/about.html')
+    return render(request, 'forum/about.html', context)
 
 
 @login_required
 def post_page(request, post_id):
     # print(post_id)
-    post = Post.objects.get(id=post_id)
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        raise Http404("No MyModel matches the given query.")
+
     replies = post.reply_set.all()
     tags = post.tags.all()
+
     context = {
         'post': post,
         'replies': replies,
@@ -87,6 +131,29 @@ def search_results(request):
     }
     # do some searching using query string, and send that to the template
     return render(request, 'forum/search.html', context)
+
+
+def create_post(request):
+    if request.method == "POST":
+        print(request.POST)
+        print(request.user)
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            post_title = form.cleaned_data.get('post_title')
+            print(f'Post just posted successfully: {post_title}!')
+            messages.success(request, f'Your post was posted successfully')
+
+        else:
+            print("Form wasn't valid")
+            messages.error(
+                request, f'Form is not validated')
+
+        return redirect('home_page')
+    else:
+        print("A GET req was made")
+        return redirect('home_page')
 
 
 def faq_page(request):
