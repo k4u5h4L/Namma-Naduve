9a571bdab6e9546d1e6522d84ff390347fadd3cb
k4u5h4L <kaushal.v.bhat@gmail.com>
2020-11-16 20:19:11 +0530

    Fix default profile pic

--- dbms_project/dbms_project/urls.py
+++ dbms_project/dbms_project/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('landing/', include('users.urls')),
     path('', include('forum.urls')),
-]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
